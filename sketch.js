53072a357237c74f55ee8a050ea4cd052996b548
Yulie <pyuliie@csu.fullerton.edu>
2024-10-22 18:16:11 -0700

    molddd

--- sketch.js
+++ sketch.js
@@ -1,8 +1,33 @@
+
+
+let molds = []; let num = 4000;
+let d; 
+
 function setup() {
   createCanvas(400, 400);
+  angleMode(DEGREES);
+  d = pixelDensity();
+  
+  for (let i=0; i<num; i++) {
+    molds[i] = new Mold();
+  } 
 }
 
 function draw() {
-  background(220);
+  background(0, 5);
+  loadPixels();
+  
+  for (let i=0; i<num; i++) {
+    if (key == "s") { // If "s" key is pressed, molds stop moving 
+      molds[i].stop = true;
+      updatePixels();
+      noLoop();
+    } else {
+      molds[i].stop = false;
+    }
+    
+    molds[i].update();
+    molds[i].display();
+  }
   
 }
